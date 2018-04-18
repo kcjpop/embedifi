9a8970b3938780979ebae62e9b55f7dcedd25338
An Cao <kcj.pop.com@gmail.com>
2018-04-18 15:37:01 +0300

    Build script for production
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+const mode = process.env.NODE_ENV
+
 module.exports = {
-  mode: process.env.NODE_ENV || 'development',
+  mode: mode || 'development',
   entry: {
     app: path.resolve(__dirname, 'src/index.js')
   },
   output: {
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
@@ -18,5 +23,9 @@ module.exports = {
       }
     ]
   },
-  plugins: [new HtmlWebpackPlugin(), new MiniCssExtractPlugin()]
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    ...(mode === 'production' ? [(new CleanWebpackPlugin(['dist']), new UglifyJSPlugin())] : [])
+  ]
 }
