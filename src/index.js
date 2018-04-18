ca178234dd684098d532b3e18a82e44d79f606d2
An Cao <kcj.pop.com@gmail.com>
2018-04-18 18:54:29 +0300

    Update style of embeded wrapper
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,17 +4,6 @@ import './index.css'
 
 const base64util = require('base64util')
 
-const data = {
-  url: 'https://tuoitre.vn/may-bay-no-dong-co-giua-khong-trung-hanh-khach-bi-hut-ra-cua-so-20180418091055045.htm',
-  domain: 'tuoitre.vn',
-  title: 'Máy bay nổ động cơ giữa không trung, hành khách bị hút ra cửa sổ',
-  description:
-    'TTO - Một hành khách đã bị hút nửa người ra khỏi cửa sổ và thiệt mạng sau khi động cơ máy bay của hãng Southwest Airlines bất ngờ nổ tung giữa trời ngày 17-4 (giờ Mỹ).',
-  image: 'https://cdn.tuoitre.vn/thumb_w/1200/2018/4/18/photo1524017514129-15240175141291673641385.jpg'
-}
-
-console.log(base64util.encode(JSON.stringify(data)))
-
 function getDataFromSearch() {
   const s = window.location.search.substr(1)
   return s.length === 0 ? null : JSON.parse(base64util.decode(s))
@@ -30,7 +19,7 @@ function App() {
   const data = getDataFromSearch()
   if (data == null) return m('h1', 'Error')
 
-  const wrapper = m('.db.center.mw5')
+  const wrapper = m('.h-100.center.mw5.flex.items-center.justify-center')
   const content = m(
     'a.flex.flex-column.br1.ma2.no-underline.card-1',
     { target: '_blank', rel: 'noopener noreferrer', href: data.url },
